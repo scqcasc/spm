57893e8d8bbdf19f0371188f8d3a80b776eeccbf
Sean Quaint <seanquaint@zohomail.com>
2025-01-15 18:32:06 -0500

    now updates db

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,32 @@ use std::error::Error;
 use std::path::PathBuf;
 use std::fs;
 use dirs::home_dir;
-use slint::ToSharedString;
+use slint::{ComponentHandle, ToSharedString};
 
 
 slint::include_modules!();
 
-pub fn set_pass(ui: AppWindow)  {
+pub fn set_pass(ui: AppWindow, length: i32)  {
     let p = password::Password {
         password_type:password::PasswordType::Complex,
-        password_length: 15,
+        password_length: length,
     };
     let passphrase = p.get_a_password();
     ui.set_passphrase(passphrase.to_shared_string());
 }
 
+fn get_config_dir(home: PathBuf) -> PathBuf {
+    println!("User's home directory: {}", home.display());
+    let mut config_path = PathBuf::from(home);
+    config_path.push(".local");
+    config_path.push("share");
+    config_path.push("spm");
+    config_path
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     if let Some(home) = home_dir() {
-        println!("User's home directory: {}", home.display());
-        let mut config_path = PathBuf::from(home);
-        config_path.push(".local");
-        config_path.push("share");
-        config_path.push("spm");
-
+        let mut config_path: PathBuf = get_config_dir(home);
         // make sure the config_dir exists
         if let Ok(_) = fs::create_dir_all(config_path.clone()) {
             config_path.push("spm.db");
@@ -48,29 +52,72 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Could not determine the home directory.");
     }
     let ui = AppWindow::new()?;
-    set_pass(ui.clone_strong());
+    let ui_c = ui.clone_strong();
+
+    set_pass(ui.clone_strong(), 25);
     
     ui.on_new_pass_clicked ({
         let ui_handle = ui.as_weak();
         move || {
             let ui = ui_handle.unwrap();
-            set_pass(ui);
+            let length = ui.get_passlength();
+            set_pass(ui, length);
         }
     });
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
+    ui.clone_strong().on_update_db_clicked(move || {
+        if let Some(home) = home_dir() {
+            let mut config_path: PathBuf = get_config_dir(home);
+            config_path.push("spm.db");
+            let data = db::PassEntry {
+                url: ui.get_url().to_string(),
+                id: 0,
+                username: ui.get_user_name().to_string(),
+                passphrase: ui.get_passphrase().to_string(),
+                notes: ui.get_notes().to_string(),
+            };
+            match db::insert_data(&data, &config_path) {
+                Ok(_) => {
+                    println!("{}", "ok");
+                    // let md = MessageDialog::new(
+                    //     None::<&Window>, 
+                    //     DialogFlags::empty(),
+                    //      MessageType::Info,
+                    //     ButtonsType::Ok, 
+                    //     "Entry updated");
+                    // md.run();
+                    // md.close();
+                },
+                Err(e) => {
+                    let mut msg : String = "Entry failed: ".to_owned();
+                    msg.push_str(&e.to_string());
+                    panic!("{}", msg);
+                    // let md = MessageDialog::new(
+                    //     None::<&Window>, 
+                    //     DialogFlags::empty(),
+                    //      MessageType::Error,
+                    //     ButtonsType::Ok, 
+                    //     &msg);
+                    // md.run();
+                    // md.close();
+                }
+
+            };
+        }
+    });
+    ui_c.on_request_increase_value({
+        let ui_handle = ui_c.as_weak();
         move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
         }
     });
 
-    ui.on_exit(move || {
+    ui_c.on_exit(move || {
         std::process::exit(0);
     });
         
-    ui.run()?;
+    ui_c.run()?;
 
     Ok(())
 }
