481d44fe99a3085f349b50ce9f70c6c569c79d12
Sean Quaint <seanquaint@zohomail.com>
2025-01-15 14:35:50 -0500

    added a basic gui and can generate passwords

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,19 @@ use std::error::Error;
 use std::path::PathBuf;
 use std::fs;
 use dirs::home_dir;
+use slint::ToSharedString;
+
 
 slint::include_modules!();
+
+pub fn set_pass(ui: AppWindow)  {
+    let p = password::Password {
+        password_type:password::PasswordType::Complex,
+        password_length: 15,
+    };
+    let passphrase = p.get_a_password();
+    ui.set_passphrase(passphrase.to_shared_string());
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     if let Some(home) = home_dir() {
@@ -37,6 +48,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Could not determine the home directory.");
     }
     let ui = AppWindow::new()?;
+    set_pass(ui.clone_strong());
+    
+    ui.on_new_pass_clicked ({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            set_pass(ui);
+        }
+    });
 
     ui.on_request_increase_value({
         let ui_handle = ui.as_weak();
@@ -46,6 +66,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
+    ui.on_exit(move || {
+        std::process::exit(0);
+    });
+        
     ui.run()?;
 
     Ok(())
